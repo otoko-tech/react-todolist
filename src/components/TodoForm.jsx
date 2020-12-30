035cbd088ad3b5caa62738dbae41b88c106215b5
Tedi Julianto <75712213+otoko-tech@users.noreply.github.com>
2020-12-30 12:37:29 +0700

    conditional rendering

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -12,6 +12,12 @@ const TodoForm = ({ addTodo, showAdd }) => {
       return;
     }
 
+    if (value.length > 40) {
+      alert("Tidak boleh lebih dari 40 karakter");
+      setValue("");
+      return;
+    }
+
     addTodo(value);
     setValue("");
   };
