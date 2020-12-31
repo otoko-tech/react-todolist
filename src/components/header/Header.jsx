d87679d23b006945389843efe509752d2cc4fb54
Tedi Julianto <75712213+otoko-tech@users.noreply.github.com>
2020-12-31 22:21:04 +0700

    membuat emotion css pada button dan header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
 import PropTypes from "prop-types";
+import { useTheme } from "emotion-theming";
 
 import Button from "../button/Button";
-import styles from "./header.module.css";
+// import styles from "./header.module.css";
+import * as styles from "./header.styles";
 
 const Header = ({ showAddToggle, showAdd, clearTodos }) => {
+  const theme = useTheme();
+
   return (
-    <section className={styles.header}>
+    // <section className={styles.header}>
+    //   <Button text={showAdd ? "Finish" : "Add"} onClick={showAddToggle} />
+    //   <h1 className={styles.headerTitle}>Todo Lists</h1>
+    //   <Button text="Clear" onClick={clearTodos} color="red" align="right" />
+    // </section>
+    <section css={styles.header}>
       <Button text={showAdd ? "Finish" : "Add"} onClick={showAddToggle} />
-      <h1 className={styles.headerTitle}>Todo Lists</h1>
+      <h1 css={styles.headerTitle(theme)}>Todo Lists</h1>
       <Button text="Clear" onClick={clearTodos} color="red" align="right" />
     </section>
   );
