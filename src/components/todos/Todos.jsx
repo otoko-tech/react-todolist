dfa2d03d9a1a1409e3f89b61ec4a03b9151b55ce
Tedi Julianto <75712213+otoko-tech@users.noreply.github.com>
2021-01-27 12:11:21 +0700

    implement layout ke dalam todos component

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -3,31 +3,35 @@ import { jsx } from "@emotion/core";
 import PropTypes from "prop-types";
 
 import Todo from "../todo/Todo";
+import Container from "../../layout/Container";
+
 // import styles from "./todos.module.css";
 import * as styles from "./todos.styles";
 
 const Todos = ({ todos, completeTodo }) => {
   return (
-    <section css={styles.todos}>
-      {todos.length > 0 &&
-        todos.map((todo, index) => {
-          return (
-            <Todo
-              key={index}
-              text={todo.text}
-              isCompleted={todo.isCompleted}
-              completeTodo={completeTodo}
-              index={index}
-            />
-          );
-        })}
+    <section className="todos-component">
+      <Container flexDirection="column" minHeight="500px">
+        {todos.length > 0 &&
+          todos.map((todo, index) => {
+            return (
+              <Todo
+                key={index}
+                text={todo.text}
+                isCompleted={todo.isCompleted}
+                completeTodo={completeTodo}
+                index={index}
+              />
+            );
+          })}
 
-      {todos.length === 0 && (
-        <div css={styles.todoPlaceholderText}>
-          Klik tombol <span css={styles.addButtonPlaceholderText}>ADD</span> di
-          atas untuk menambah todo baru
-        </div>
-      )}
+        {todos.length === 0 && (
+          <div css={styles.todoPlaceholderText}>
+            Klik tombol <span css={styles.addButtonPlaceholderText}>ADD</span>{" "}
+            di atas untuk menambah todo baru
+          </div>
+        )}
+      </Container>
     </section>
   );
 };
--- src/layout/Container.jsx
+++ src/layout/Container.jsx
@@ -9,7 +9,8 @@ const Container = ({
   justifyContent,
   alignItems,
   alignContent,
-  height
+  height,
+  minHeight
 }) => {
   const containerStyles = css`
     display: flex;
@@ -19,6 +20,7 @@ const Container = ({
     align-items: ${alignItems};
     align-content: ${alignContent};
     height: ${height};
+    min-height: ${minHeight};
   `;
   return (
     <div className="flex-container" css={containerStyles}>
@@ -33,7 +35,8 @@ Container.defaultProps = {
   justifyContent: "flex-start",
   alignItems: "stretch",
   alignContent: "stretch",
-  height: "auto"
+  height: "auto",
+  minHeight: "initial"
 };
 
 Container.propTypes = {
@@ -87,7 +90,8 @@ Container.propTypes = {
     "first basline",
     "last baseline"
   ]),
-  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  minHeight: PropTypes.string
 };
 
 export default Container;
