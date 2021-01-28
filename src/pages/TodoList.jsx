f2edaae7d52e712977eb2a3ccb273bb33048c19b
Tedi Julianto <75712213+otoko-tech@users.noreply.github.com>
2021-01-28 12:24:41 +0700

    custom hooks for usestate localstorage

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
+import useStateWithLocalStorage from "../hooks/useStateWithLocalStorage";
 import Paper from "../components/paper/PaperClass";
 import Header from "../components/header/HeaderClass";
 import TodoForm from "../components/todoform/TodoFormClass";
@@ -7,21 +7,8 @@ import Todos from "../components/todos/TodosClass";
 import Container from "../layout/Container";
 
 const TodoList = () => {
-  // const [todos, setTodos] = useState([
-  //   { text: "Learning React!", isCompleted: false },
-  //   { text: "Learning React Hooks!", isCompleted: false },
-  //   { text: "Learning styling in React!", isCompleted: false }
-  // ]);
-
-  const [todos, setTodos] = useState(
-    JSON.parse(localStorage.getItem("todos")) || []
-  );
-
+  const [todos, setTodos] = useStateWithLocalStorage("todos");
   const [showAdd, setShowAdd] = useState(false);
-
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
 
   const addTodo = (value) => {
     if (todos.length < 10) {
